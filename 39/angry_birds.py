9eadd25854df24b629940a86cdd8eb391a3dfa1b
Luca Ryholt <lucaryholt@gmail.com>
2020-09-24 15:39:39 +0200

    exercises done

---
+++ 39/angry_birds.py
@@ -0,0 +1,82 @@
+from os import system, name
+import random
+import sys
+
+class Bird:
+    def __init__(self):
+        self.icon = '🐥'
+        self.x_loc = random.randint(0, 9)
+        self.y_loc = random.randint(0, 9)
+
+    def move(self, input):
+        move = input.lower()
+        if(move == 'w'):
+            self.x_loc -= 1
+        elif(move == 'a'):
+            self.y_loc -= 1
+        elif(move == 's'):
+            self.x_loc += 1
+        elif(move == 'd'):
+            self.y_loc += 1
+
+class Pig:
+    def __init__(self):
+        self.icon = '🐷'
+        self.x_loc = random.randint(0, 9)
+        self.y_loc = random.randint(0, 9)
+
+class BoardPiece:
+    def __init__(self):
+        self.icon = '🌱'
+
+class Board:
+
+    def __init__(self, bird, pig):
+        self.bird = bird
+        self.pig = pig
+        self.boardArray = []
+
+    def initBoard(self):
+        self.boardArray = []
+        for i in range(0,10):
+            temp = []
+            for j in range(0,10):
+                temp.append(BoardPiece().icon)
+            self.boardArray.append(temp)
+
+    def printBoard(self):
+        boardString = ''
+        for p in self.boardArray:
+            for pp in p:
+                boardString = boardString + ' ' +  pp
+            boardString = boardString + '\n'
+        print(boardString)
+
+    def updateBoard(self):
+        self.initBoard()
+        self.boardArray[self.pig.x_loc][self.pig.y_loc] = self.pig.icon
+        self.boardArray[self.bird.x_loc][self.bird.y_loc] = self.bird.icon
+        if(self.gameWon()):
+            print('Game won!')
+            sys.exit(1)
+        self.clear()
+        self.printBoard()
+
+    def clear(self):
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
+
+    def gameWon(self):
+        return self.pig.x_loc == self.bird.x_loc and self.pig.y_loc == self.bird.y_loc
+
+b = Bird()
+p = Pig()
+
+board = Board(b, p)
+
+while(True):
+    board.updateBoard()
+    move = input()
+    b.move(move)
--- 39/oop.py
+++ 39/oop.py
@@ -1,2 +1,39 @@
-class Person:
-    pass
+#Exercise 1
+
+class Bank:
+    def __init__(self, *args):
+        self.accounts = list(args)
+
+    def addAccount(self, account):
+        self.accounts.append(account)
+
+    def __str__(self):
+        return '# of accounts: ' + str(len(self.accounts))
+
+class Account:
+    def __init__(self, customer, balance):
+        self.customer = customer
+        self.balance = balance
+
+    def __str__(self):
+        return 'Customer: ' + self.customer.name + ', balance: ' + str(self.balance)
+
+class Customer:
+    def __init__(self, name, age, creditScore):
+        self.name = name
+        self.age = age
+        self.creditScore = creditScore
+
+    def __str__(self):
+        return 'Name: 📱' + self.name + ', age: ' + str(self.age) + ', credit score: ' + str(self.creditScore)
+
+#Start data
+billy = Customer('Billy', 45, 0)
+bobby = Customer('Bobby', 12, 500)
+
+billy_account = Account(billy, 400)
+bobby_account = Account(bobby, 500)
+
+bank = Bank(billy_account, bobby_account)
+
+#bank.addAccount(billy_account)
